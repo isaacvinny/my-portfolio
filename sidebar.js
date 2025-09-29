3a9b0a3bbe5a090cba2beca72796ef184431e2bd
Isaac Vincent Kofi <isaacvincent076@gmail.com>
2025-09-29 02:27:08 +0100

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+function openSideBar(){
+    let shownavbar = document.querySelector('.side-bar')
+    shownavbar.style.right = "0"
+}
+function closSibeBar(){
+    let hidenavbar = document.querySelector('.side-bar') 
+    hidenavbar.style.right = "-100%"
+}
